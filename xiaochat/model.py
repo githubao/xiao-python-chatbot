b441c9b2cdbe18dbaf98d5fa72bae68a56847b58
BaoQiang <mailbaoqiang@gmail.com>
2017-04-24 21:47:30 +0800

    tfidf predict

--- xiaochat/model.py
+++ xiaochat/model.py
@@ -11,8 +11,11 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-test_df = None
+train_df = pd.read_csv('data/train.csv')
+test_df = pd.read_csv('data/test.csv')
+validation_df = pd.read_csv('data/valid.csv')
 
 
 def evaluate_recall(y, y_test, k=1):
@@ -29,14 +32,48 @@ def predict_random(context, utterances):
 
 
 def predict1():
-    y_random = [predict_random(test_df.Context[x], test_df.iloc[x, 1:].valus) for x in range(len(test_df))]
+    '''
+    Recall @ (1, 10): 0.103805
+    Recall @ (2, 10): 0.20777
+    Recall @ (5, 10): 0.504915
+    Recall @ (10, 10): 1
+    '''
+    y_random = [predict_random(test_df.Context[x], test_df.iloc[x, 1:].values) for x in range(len(test_df))]
     y_test = np.zeros(len(y_random))
     for n in [1, 2, 5, 10]:
-        print('Recall @ ({}, 10): {:g}'.format(n, evaluate_recall(y_random, y_test), n))
+        print('Recall @ ({}, 10): {:g}'.format(n, evaluate_recall(y_random, y_test, n)))
+
+
+class TfIdfPredictor:
+    def __init__(self):
+        self.vectorizer = TfidfVectorizer()
+
+    def train(self, data):
+        self.vectorizer.fit(np.append(data.Context.values, data.Utterance.values))
+
+    def predict(self, context, utterances):
+        # convert context and utterrances into tfidf vector
+        vector_context = self.vectorizer.transform([context])
+        vector_doc = self.vectorizer.transform(utterances)
+        # the dot product measures the similarity of the resulting vectors
+        result = np.dot(vector_doc, vector_context.T).todense()
+        result = np.asarray(result).flatten()
+        # sort by top results and return the indices in descending order
+        return np.argsort(result, axis=0)[::-1]
+
+
+def predict2():
+    pred = TfIdfPredictor()
+    pred.train(train_df)
+    y = [pred.predict(test_df.Context[x], test_df.iloc[x, 1:].values) for x in range(len(test_df))]
+    y_test = np.zeros(len(y))
+    for n in [1, 2, 5, 10]:
+        print('Recall @ ({}, 10): {:g}'.format(n, evaluate_recall(y, y_test, n)))
 
 
 def main():
-    predict1()
+    # predict1()
+    predict2()
 
 
 if __name__ == '__main__':
